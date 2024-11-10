c6366a153ff22c3a73e6c6c96705466257643833
Chris Yates <beastbytes@users.noreply.github.com>
2024-11-10 21:19:25 +0000

    Add StepHandlerTrait.php

---
+++ src/StepHandlerTrait.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @copyright Copyright © 2024 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
+
+declare(strict_types=1);
+
+namespace BeastBytes\Wizard;
+
+use BeastBytes\Wizard\Event\Step as StepEvent;
+
+trait StepHandler
+{
+    /** Step event handler */
+    public function stepHandler(StepEvent $event): void
+    {
+        $step = $event
+            ->getWizard()
+            ->getCurrentStep()
+        ;
+
+        if (method_exists($this, $step)) {
+            $this->$step($event);
+            $event->stopPropagation();
+        }
+    }
+}
